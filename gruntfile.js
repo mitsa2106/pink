292f2798d42d7f28c982223a0bb1218ffc9f2a0b
Lena Mitskevich <mitsa2106@gmail.com>
2020-01-29 01:36:39 +0300

    изменения (погружение в автоматизацию): - конфигурация сборки стилей на grunt; - минификация CSS на grunt; - минификация изображений на grunt; - создание изображеий в формате WebP на grunt; - SVG-спрайт на grunt; - сборка проектов на Grunt

--- gruntfile.js
+++ gruntfile.js
@@ -1,60 +1,145 @@
 "use strict";
 
-module.exports = function(grunt) {
-    grunt.loadNpmTasks("grunt-contrib-less");
-	grunt.loadNpmTasks("grunt-browser-sync");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-postcss");
+module.exports = function (grunt) {
+  // grunt.loadNpmTasks("grunt-contrib-less");
+  // grunt.loadNpmTasks("grunt-browser-sync");
+  // grunt.loadNpmTasks("grunt-contrib-watch");
+  // grunt.loadNpmTasks("grunt-postcss");
 
-    grunt.initConfig({
-        less: {
-	        style: {
-	            files: {
-		            "source/css/style.css": "source/less/style.less",
-		            "source/css/outlines.css": "source/less/outlines.less"
-		        }
-	        }
-	    },
+  require("load-grunt-tasks")(grunt);
 
-
-		postcss: {
-			style: {
-				options: {
-					processors: [
-						require("autoprefixer")()
-					]
-				},
-				src: "source/css/*.css"
-			}
-		},
-
-		browserSync: {
-			server: {
-				bsFiles: {
-					src: [
-						"source/*.html",
-						"source/css/*.css"
-					]
-				},
-				options: {
-					server: "source/.",
-					watchTask: true,
-					notify: false,
-					open: true,
-					cors: true,
-					ui: false
-				}
-			}
-		},
-
-		watch: {
-			style: {
-				files: ["source/less/**/*.less"],
-				tasks: ["less", "postcss"]
-			}
-		}
-
+  grunt.initConfig({
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "fonts/**/*.{woff,woff2}",
+            "img/**",
+            "js/**"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+    clean: {
+      build: ["build"]
+    },
+    less: {
+      style: {
+        files: {
+          "build/css/style.css": "source/less/style.less",
+          "build/css/outlines.css": "source/less/outlines.less"
+        }
+      }
+    },
+    postcss: {
+      style: {
+        options: {
+          processors: [
+            require("autoprefixer")()
+          ]
+        },
+        src: "build/css/*.css"
+      }
+    },
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+    svgstore: {
+      options: {
+        includeTitleElement: false
+      },
+      sprite: {
+        files: {
+          "build/img/sprite.svg": ["source/img/icon-*.svg"]
+        }
+      }
+    },
+    posthtml: {
+      options: {
+        use: [
+          require("posthtml-include")()
+        ]
+      },
+      html: {
+        files: [{
+          expand: true,
+          src: ["source/*.html"],
+          dest: "build"
+        }]
+      }
+    },
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3,
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg,svg}"]
+        }]
+      }
+    },
+    cwebp: {
+      images: {
+        options: {
+          q: 90
+        },
+        files: [{
+          expand: true,
+          src: ["source/img/**/*.{png,jpg}"]
+        }]
+      }
+    },
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "build/*.html",
+            "build/css/*.css"
+          ]
+        },
+        options: {
+          server: "build/.",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+    watch: {
+      html: {
+        files: ["source/*.html"],
+        tasks: ["posthtml"]
+      },
+      style: {
+        files: ["source/less/**/*.less"],
+        tasks: ["less", "postcss", "csso"]
+      }
+    }
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "less",
+    "postcss",
+    "csso",
+    "svgstore",
+    "posthtml"
+  ]);
 };
